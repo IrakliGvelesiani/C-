731417290ebb8c6c34a46d61eced671d834b9afa
IrakliGvelesiani <104423165+IrakliGvelesiani@users.noreply.github.com>
2023-03-14 09:42:57 +0400

    დავალება 2

---
+++ Konstruqtori_the_Sivrce_1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Konstruqtori_the_Sivrce_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int gverdi_1, gverdi_2, gverdi_3;
+            int perimetri; 
+            double fartobi;
+            gverdi_1 = int.Parse(textBox1.Text);
+            gverdi_2 = int.Parse(textBox2.Text);
+            gverdi_3 = int.Parse(textBox3.Text);
+            Samkutxedi obj_samkutxedi = new Samkutxedi(gverdi_1, gverdi_2, gverdi_3);
+            obj_samkutxedi.Gamotana_1(label1);
+
+        }
+
+    }
+}
---
+++ Konstruqtori_the_Sivrce_1/Samkutxedi.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Konstruqtori_the_Sivrce_1
+{
+    class Samkutxedi
+    {
+       public int gverdi_1, gverdi_2, gverdi_3;
+        private int perimetri;
+        double fartobi;
+        public Samkutxedi(int gverdi_1 , int gverdi_2, int gverdi_3)
+        {
+            this.gverdi_1 = gverdi_1;
+            this.gverdi_2 = gverdi_2;
+            this.gverdi_3 = gverdi_3;
+            perimetri = this.gverdi_1 + this.gverdi_2 + this.gverdi_3;
+            fartobi = this.gverdi_1 * this.gverdi_2 / 2.0;
+        }
+        public void Gamotana_1(Label par_1)
+        {
+            par_1.Text = "პერიმეტრი = " + perimetri.ToString() + "\n" +
+                "ფართობი = " + fartobi.ToString();
+        }
+    }
+}
